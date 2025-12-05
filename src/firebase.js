1bd7fa6559dc28a8e92b377c3cd33dc97bab2db7
William <will.mancao@gmail.com>
2025-12-05 19:41:21 +0800

    Merge remote changes

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOCqRqIwl_Mhq3vtlbb0nl-Ijn8SVWxfQ",
-  authDomain: "monmon-89c30.firebaseapp.com",
-  projectId: "monmon-89c30",
-  storageBucket: "monmon-89c30.firebasestorage.app",
-  messagingSenderId: "379585627840",
-  appId: "1:379585627840:web:4757ed422dc5f006d64f25"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
